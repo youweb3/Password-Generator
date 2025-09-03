66eb8b430204ef55d7f8006932bcab2fbb1b02d9
Kamran <Yousef.shaikhan@gmail.com>
2025-09-03 22:51:32 +0100

    first_commit
    created a password generator

---
+++ password-genarator/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import PasswordGenerator from './Component/PasswordGenerator';
+
+function App() {
+
+  return (
+    <>
+    <PasswordGenerator/>
+    </>
+  );
+};
+
+export default App;
